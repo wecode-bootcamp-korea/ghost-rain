44f9e9222c69e896291a131a8b10d31346634bc9
Yeri-Kim <yerikim1028@gmail.com>
2022-03-15 22:00:02 +0900

    랜덤한 위치로 유령 시작하기

--- js/ghost.js
+++ js/ghost.js
@@ -1,9 +1,13 @@
+function randomRange(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
 function create() {
   const ghostElement = document.createElement('div');
 
   ghostElement.style.position = 'absolute';
   ghostElement.style.top = '0px';
-  ghostElement.style.left = '50%';
+  ghostElement.style.left = randomRange(0, BG_WIDTH - GHOST_WIDTH) + 'px';
 
   ghostElement.style.width = GHOST_WIDTH + 'px';
   ghostElement.style.height = GHOST_HEIGHT + 'px';
