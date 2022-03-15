fa7903f4a840faebf97923b32bdef235751aa898
Yeri-Kim <yerikim1028@gmail.com>
2022-03-15 22:44:33 +0900

    유령비 내리기

--- js/ghost.js
+++ js/ghost.js
@@ -2,11 +2,12 @@ function randomRange(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-function create() {
-  const ghostElement = document.createElement('div');
+let enemyTop = 0;
+const ghostElement = document.createElement('div');
 
+function create() {
   ghostElement.style.position = 'absolute';
-  ghostElement.style.top = '0px';
+  ghostElement.style.top = enemyTop + 'px';
   ghostElement.style.left = randomRange(0, BG_WIDTH - GHOST_WIDTH) + 'px';
 
   ghostElement.style.width = GHOST_WIDTH + 'px';
@@ -14,6 +15,15 @@ function create() {
   ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
 
   bg.appendChild(ghostElement);
+
+  window.requestAnimationFrame(move);
+}
+
+function move() {
+  enemyTop++;
+  ghostElement.style.top = enemyTop + 'px';
+
+  window.requestAnimationFrame(move);
 }
 
 create();
