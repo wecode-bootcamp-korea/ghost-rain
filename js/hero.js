9a033738ebe6e6b03e74b3d5c259b3fdc7b192fa
Yeri-Kim <yerikim1028@gmail.com>
2022-03-15 21:41:20 +0900

    배경 밖으로 나가지 못하도록 용사 막기

--- js/hero.js
+++ js/hero.js
@@ -4,5 +4,7 @@ function setLeft(left) {
   const currentLeft = Number(getComputedStyle(heroElement).left.split('px')[0]);
   const newleft = currentLeft + left;
 
+  if (newleft > BG_WIDTH - HERO_WIDTH || newleft < 0) return;
+
   heroElement.style.left = newleft + 'px';
 }
